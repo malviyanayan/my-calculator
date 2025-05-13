8651900235fe159d27985d7f554f2313909e398d
nayan malviya <naymalviya@gmail.com>
2025-05-13 10:51:07 +0530

    version 1.4

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/my-calculator/',  // 👈 yeh line bahut important hai
+  base: '/my-calculator/',
   plugins: [react()],
 })
